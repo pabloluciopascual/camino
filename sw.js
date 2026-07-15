1b8098690da71ed7e86f74cfefb356db7a74d119
pabloluciopascual <pablo.lucio.pascual@gmail.com>
2026-07-15 08:53:51 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+// Service worker: cachea la app para que funcione sin conexión.
+// Si cambias index.html, sube VERSION (v1 -> v2) para forzar la actualización.
+const VERSION = "camino-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(VERSION).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== VERSION).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Estrategia: red primero para el HTML (para recibir actualizaciones),
+// caché primero para el resto (iconos, manifest, fuentes ya cacheadas).
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.mode === "navigate") {
+    e.respondWith(
+      fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(VERSION).then((c) => c.put("./index.html", copy));
+          return res;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(req).then(
+      (hit) =>
+        hit ||
+        fetch(req).then((res) => {
+          // cachear también recursos externos correctos (p. ej. fuentes de Google)
+          if (res.ok || res.type === "opaque") {
+            const copy = res.clone();
+            caches.open(VERSION).then((c) => c.put(req, copy));
+          }
+          return res;
+        })
+    )
+  );
+});
